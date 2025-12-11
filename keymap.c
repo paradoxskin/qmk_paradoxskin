e38236e674459cc1fd13fbf6938b6da3008f28e7
paradoxskin <1312269430@qq.com>
2025-12-12 00:11:46 +0800

    support qk61
    Signed-off-by: paradoxskin <1312269430@qq.com>

--- keymap.c
+++ keymap.c
@@ -1,7 +1,22 @@
 #include QMK_KEYBOARD_H
 
-/* qmk keycode diff */
-#ifndef MS_BTN1
+/* keyboard diff */
+#ifdef KEYBOARD_keychron_v4
+#define HAS_MAC
+#define ENABLE_MATRIX
+#define _LAYOUT LAYOUT_ansi_61
+#endif
+
+#ifdef KEYBOARD_keychron_k9_pro_ansi_rgb
+#define HAS_MAC
+#define ENABLE_BLUETOOTH
+#define ENABLE_MATRIX
+#define _LAYOUT LAYOUT_61_ansi
+#define KC_BTU BT_USB
+#define KC_BT1 BT_HST1
+#define KC_BT2 BT_HST2
+#define KC_BT3 BT_HST3
+#define KC_BT4 XXXXXXX
 #define MS_BTN1 KC_BTN1
 #define MS_BTN2 KC_BTN2
 #define MS_UP KC_MS_U
@@ -20,25 +35,6 @@
 #define RM_HUEU RGB_HUI
 #endif
 
-/* keyboard diff */
-#ifdef KEYBOARD_keychron_v4
-#define HAS_MAC
-#define ENABLE_MATRIX
-#define _LAYOUT LAYOUT_ansi_61
-#endif
-
-#ifdef KEYBOARD_keychron_k9_pro_ansi_rgb
-#define HAS_MAC
-#define ENABLE_BLUETOOTH
-#define ENABLE_MATRIX
-#define _LAYOUT LAYOUT_61_ansi
-#define KC_BTU BT_USB
-#define KC_BT1 BT_HST1
-#define KC_BT2 BT_HST2
-#define KC_BT3 BT_HST3
-#define KC_BT4 XXXXXXX
-#endif
-
 #ifdef KEYBOARD_keychron_k12_pro_ansi_white
 #define HAS_MAC
 #define ENABLE_BLUETOOTH
@@ -48,14 +44,22 @@
 #define KC_BT2 BT_HST2
 #define KC_BT3 BT_HST3
 #define KC_BT4 XXXXXXX
-#undef RM_TOGG
-#undef RM_NEXT
-#undef RM_VALU
-#undef RM_SATU
+#define MS_BTN1 KC_BTN1
+#define MS_BTN2 KC_BTN2
+#define MS_UP KC_MS_U
+#define MS_DOWN KC_MS_D
+#define MS_LEFT KC_MS_L
+#define MS_RGHT KC_MS_R
+#define MS_WHLU KC_WH_U
+#define MS_WHLD KC_WH_D
+#define MS_WHLL KC_WH_L
+#define MS_WHLR KC_WH_R
 #define RM_TOGG BL_TOGG
 #define RM_NEXT BL_STEP
+#define RM_SPDU RGB_SPI
 #define RM_VALU BL_UP
 #define RM_SATU BL_DOWN
+#define RM_HUEU RGB_HUI
 #endif
 
 #ifdef KEYBOARD_annepro2_c18
@@ -66,6 +70,25 @@
 #define KC_BT2 KC_AP2_BT2
 #define KC_BT3 KC_AP2_BT3
 #define KC_BT4 KC_AP2_BT4
+#endif
+
+#ifdef KEYBOARD_qk61
+#include "common/rdmctmzt_common.h"
+#define ENABLE_BLUETOOTH
+#define _LAYOUT LAYOUT_60_ansi
+#define KC_BTU MD_USB
+#define KC_BT1 MD_BLE1
+#define KC_BT2 MD_BLE2
+#define KC_BT3 MD_BLE3
+#define KC_BT4 MD_24G
+#endif
+
+#ifndef ENABLE_BLUETOOTH
+#define KC_BTU XXXXXXX
+#define KC_BT1 XXXXXXX
+#define KC_BT2 XXXXXXX
+#define KC_BT3 XXXXXXX
+#define KC_BT4 XXXXXXX
 #endif
 
 /* func declare */
@@ -159,12 +182,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TD(TD_TH_LC_F1),     MO(_FN2), KC_LGUI,                KC_ODD_SPACE,                 KC_RALT, LT(_FN1,KC_LEFT), LT(_FN2,KC_DOWN), LT(_FNX,KC_RIGHT)
     ),
     [_FN1] = _LAYOUT(
-#ifdef ENABLE_BLUETOOTH
-        KC_BTU,  KC_BT1,  KC_BT2,  KC_BT3,  KC_BT4,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-#else
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-#endif
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RM_HUEU, RM_SATU, RM_VALU,
+        XXXXXXX, KC_BT1,  KC_BT2,  KC_BT3,  KC_BT4,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_BTU,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RM_HUEU, RM_SATU, RM_VALU,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RM_SPDU,          RM_NEXT,
         KC_LSFT,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          RM_TOGG,
         XXXXXXX, XXXXXXX, KC_PSCR,                            KC_LSFT,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
@@ -194,10 +213,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // TO layer
     [_TN1] = _LAYOUT(
         KC_BSPC, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_7,    KC_8,        KC_9,           KC_KP_PLUS,  _______,  _______, _______, _______, _______, _______, _______, MS_RGHT, _______, _______,
-        KC_4,    KC_5,        KC_6,           KC_KP_PLUS,  _______,  _______, _______, _______, _______, _______, MS_LEFT, MS_UP,            MS_BTN2,
-        KC_1,                 KC_2,           KC_3,        KC_ENTER, _______, _______, _______, _______, _______, MS_DOWN, _______,          _______,
-        KC_DOT,  KC_0,        KC_0,                                           KC_ENTER,                           MS_BTN1, _______, _______, LT(_FNY, KC_RIGHT)
+        KC_7,    KC_8,        KC_9,           KC_KP_PLUS,  _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_4,    KC_5,        KC_6,           KC_KP_PLUS,  _______,  _______, MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, MS_BTN2, _______,          _______,
+        KC_1,                 KC_2,           KC_3,        KC_ENTER, _______, _______, _______, _______, _______, _______, _______,          _______,
+        KC_DOT,  KC_0,        KC_0,                                           MS_BTN1,                           _______, _______, _______, LT(_FNY, KC_RIGHT)
     ),
     [_TN2] = _LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
